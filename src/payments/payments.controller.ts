60d18dd9761f47ce7d79b207ee0c6d8cbe29783f
Keneth <kenethcubas2002@unitec.edu>
2026-02-12 23:20:24 -0600

    refactor createPaymentSession to accept PaymentSessionDto as a parameter

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,17 +1,19 @@
 import {
+  Body,
   Controller,
   Get,
   Post,
 } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
+import { PaymentSessionDto } from './dto/payment-session.dto';
 
 @Controller('payments')
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
 
   @Post('create-payment-session')
-  createPaymentSession() {
-    return this.paymentsService.createPaymentSession();
+  createPaymentSession(@Body() paymentSessionDto: PaymentSessionDto) {
+    return this.paymentsService.createPaymentSession(paymentSessionDto);
   }
 
   @Get('success')
