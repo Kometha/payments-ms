785331d9548e8387121cb43e0240446601c6b36b
Keneth <kenethcubas2002@unitec.edu>
2026-02-14 14:21:23 -0600

    refactor stripeWebhook method to remove unnecessary Request type annotation

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { PaymentSessionDto } from './dto/payment-session.dto';
-import type { Request, Response } from 'express';
+import type { Response } from 'express';
 
 @Controller('payments')
 export class PaymentsController {
@@ -23,7 +23,7 @@ export class PaymentsController {
   }
 
   @Post('webhook')
-  async stripeWebhook(@Req() req: Request, @Res() res: Response) {
+  async stripeWebhook(@Req() req, @Res() res: Response) {
     return this.paymentsService.stripeWebhook(req, res);
   }
 }
